f59afc2f40238f4d9d1193f44f26d6bfc0f57539
Yunan Helmy <m.yunan.helmy@gmail.com>
2019-11-29 21:34:02 +0700

    [Patch] fix when source_path is symlink dir

--- package/rules/file.js
+++ package/rules/file.js
@@ -1,6 +1,10 @@
 const { join } = require('path')
 const { source_path: sourcePath, static_assets_extensions: fileExtensions } = require('../config')
 
+const fs = require('fs')
+const isSymLink = fs.lstatSync(sourcePath.split('/').shift()).isSymbolicLink()
+const realPath = fs.realpathSync(sourcePath.split('/').shift()) + sourcePath.replace(sourcePath.split('/').shift(), '')
+
 module.exports = {
   test: new RegExp(`(${fileExtensions.join('|')})$`, 'i'),
   use: [
@@ -8,6 +12,9 @@ module.exports = {
       loader: 'file-loader',
       options: {
         name(file) {
+          if(isSymLink){
+              return 'media' + file.replace(realPath, '')+ '[name]-[hash].[ext]'
+          }
           if (file.includes(sourcePath)) {
             return 'media/[path][name]-[hash].[ext]'
           }
